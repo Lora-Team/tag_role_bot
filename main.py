c54bb52f07ca5ab3237ea107a55243638b36b18d
Bekian <Bekian.Cat@gmail.com>
2025-10-04 14:19:10 -0500

    feat(app): made the bot
    see changes, the entire code is like 45 lines

---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        # attempt to get the respective tag role from the server
+        role = discord.Guild.get_role(after.guild, 1409425360126349483)
+        # if the role is not found
+        if not role:
+            print("role not found in server!")
+            return
+        # if the user updates their tag to "LAMP"
+        if after.primary_guild.tag == "LAMP":
+            # add the role to the user
+            await after.add_roles(role, reason="User has lamp tag")
+            print("user has lamp tag")
+        else:
+            # ensure the user does have the role
+            if not after.get_role(1409425360126349483):
+                print("user didn't have lamp role")
+                return
+            # remove the role
+            await after.remove_roles(role, reason="User removed lamp tag")
+            print("user no longer has lamp tag and the role was removed")
+
+
+# Initialize your bot
+# set intents
+intents = discord.Intents.default()
+intents.members = True
+# get token
+_ = load_dotenv()
+tok = os.getenv("TOKEN")
+if not tok:
+    os._exit(1)
+# start app
+client = MyClient(intents=intents)
+client.run(tok)
