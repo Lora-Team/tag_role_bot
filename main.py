452b49bdeacdf4b29046890ee22354a82b0f09bd
Bekian <Bekian.Cat@gmail.com>
2025-10-05 14:26:00 -0500

    feat(app): simplified logic even more
    reworked logic again so its only 2 statements, also ignored before variable

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ class MyClient(discord.Client):
         print(f"Logged in as {self.user}")
 
     async def on_member_update(self, before: discord.Member, after: discord.Member):
+        _ = before  # not used
         # attempt to get the respective tag role from the server
         role = discord.Guild.get_role(after.guild, 1409425360126349483)
         # if the role is not found, panic
         if not role:
             sys.exit("Role not found in server!")
-        # if the user updates their tag to "LAMP" then add the role
-        if after.primary_guild.tag == "LAMP":
+        has_role = after.get_role(1409425360126349483)
+        has_tag = after.primary_guild.tag == "LAMP"
+        if has_tag and not has_role:
+            # if the user updates their tag to "LAMP" then add the role
             await after.add_roles(role, reason="User has lamp tag")
             print(f"{after.name} has lamp tag, added role")
-        elif after.get_role(1409425360126349483):
+        elif has_role and not has_tag:
             # if the user has the role and their tag is not lamp then remove the role
             await after.remove_roles(role, reason="User removed lamp tag")
             print(f"{after.name} no longer has lamp tag, removed role")
