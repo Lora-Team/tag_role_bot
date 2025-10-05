85e6de20221276b759ab1967e4a93ca0004cd177
Bekian <Bekian.Cat@gmail.com>
2025-10-05 11:29:53 -0500

    feat(app): cleaning
    added back the  parameter for the on_member_update method because it threw a parameter mismatch exception. cleaned up a lot of the comments so they're more clear about what's going on. os.exit has been replaced with sys.exit cause it's safer and allows an additional message. also some of the print messages were modified slightly to be more clear about what's being done with the user.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import sys
 from dotenv import load_dotenv
 
 
@@ -8,22 +9,20 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print(f"Logged in as {self.user}")
 
-    async def on_member_update(self, after: discord.Member):
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
         # attempt to get the respective tag role from the server
         role = discord.Guild.get_role(after.guild, 1409425360126349483)
-        # if the role is not found
+        # if the role is not found, panic
         if not role:
-            print("role not found in server!")
-            return
-        # if the user updates their tag to "LAMP"
+            sys.exit("Role not found in server!")
+        # if the user updates their tag to "LAMP" then add the role
         if after.primary_guild.tag == "LAMP":
-            # add the role to the user
             await after.add_roles(role, reason="User has lamp tag")
-            print(f"{after.name} has lamp tag")
+            print(f"{after.name} has lamp tag, added role")
         elif after.get_role(1409425360126349483):
-            # if the user has the role and their tag is not lamp then remove it
+            # if the user has the role and their tag is not lamp then remove the role
             await after.remove_roles(role, reason="User removed lamp tag")
-            print(f"{after.name} no longer has lamp tag and the role was removed")
+            print(f"{after.name} no longer has lamp tag, removed role")
 
 
 # Initialize your bot
@@ -34,7 +33,7 @@ intents.members = True
 load_dotenv()
 tok = os.getenv("TOKEN")
 if not tok:
-    os._exit(1)
+    sys.exit("No env token found")
 # start app
 client = MyClient(intents=intents)
 client.run(tok)
